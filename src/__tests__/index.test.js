07fdbb3ad9e6e5f0fdae34f0b289ed1f4225c2be
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-07-15 22:49:17 +0700

    add test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -5,27 +5,22 @@ test('Шаблон', () => {
   expect(validator.validateUsername('abc_1s3j9-b')).toBeTruthy();
 });
 
-test('цифр большех', () => {
+test('цифр больше трёх', () => {
   const validator = new Validator();
-  expect(validator.validateUsername('abc123456-b')).toBeTruthy();
-});
-
-test('Шаблон', () => {
-  const validator = new Validator();
-  expect(validator.validateUsername('a123_abc-f')).toBeTruthy();
+  expect(validator.validateUsername('abc123456-b')).toBeFalsy();
 });
 
 test('Наинаеться и заканчивается цифрами', () => {
   const validator = new Validator();
-  expect(validator.validateUsername('123_abc-f')).toBeTruthy();
+  expect(validator.validateUsername('123_abc-56')).toBeFalsy();
 });
 
 test('Наинаеться и заканчивается подчёркиванием и тире', () => {
   const validator = new Validator();
-  expect(validator.validateUsername('_a123_adc-')).toBeTruthy();
+  expect(validator.validateUsername('_a123_adc-')).toBeFalsy();
 });
 
 test('Не латинские буквы', () => {
   const validator = new Validator();
-  expect(validator.validateUsername('Ля-Ля-Фа')).toBeTruthy();
+  expect(validator.validateUsername('Ля-Ля-Фа')).toBeFalsy();
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 export default class Validator {
   validateUsername(name) {
     this.name = name;
-    return (/[A-Za-z0-9\-_]+/.exec(this.name) !== -1)
-        && (!/^[A-Za-z][0-9]{3}[a-zA-Z]$/.exec(this.name) !== -1);
+    return (/^[A-Za-z][A-Za-z0-9\-_]+/.test(this.name))
+      && (/[a-zA-Z]$/.test(this.name))
+      && (!/[0-9]{4}/.test(this.name));
   }
 }
